265a58a05056eed4c0152dad02034e2f89f69a69
Maicol Matteoli <maicol.matteoli@giuneco.it>
2022-03-11 14:46:46 +0100

    add linq example

---
+++ LinqPractice/LinqPractice/FileInfoComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y) //returns -1 if x < y || 0 if x == y || 1 if x > y
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
--- LinqPractice/LinqPractice/Program.cs
+++ LinqPractice/LinqPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace LinqPractice
 {
@@ -6,7 +8,39 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\windows";
+            ShowFilesWithoutLinq(path);
+            Console.WriteLine("******************");
+            ShowFilesWithLinq(path);
+            Console.ReadLine();
+        }
+
+        private static void ShowFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach(var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+        }
+
+
+        //search files without linq
+        public static void ShowFilesWithoutLinq(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+
+            Array.Sort(files, new FileInfoComparer()); //sort the array of files
+            for(int i=0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
         }
     }
+
+
 }
