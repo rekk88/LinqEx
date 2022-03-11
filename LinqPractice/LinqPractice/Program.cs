8954e6f4cea4f960bbd35037d370d83f3343b89f
Maicol Matteoli <maicol.matteoli@giuneco.it>
2022-03-11 17:03:51 +0100

    add Func

--- LinqPractice/LinqPractice/Program.cs
+++ LinqPractice/LinqPractice/Program.cs
@@ -8,6 +8,16 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
+            #region Func
+            //this tells me that f is a function that takes an int and returns an int
+            Func<int, int> Square = x => x * x;
+            //this function takes 2 integers value as parameters and returns a int as a result
+            Func<int, int, int> Prod = (x, y) => (x * y);
+
+            Console.WriteLine(Square(2));
+            Console.WriteLine(Prod(2, 5));
+            Console.WriteLine("*******************");
+            #endregion
             string path = @"C:\windows";
             ShowFilesWithoutLinq(path);
             Console.WriteLine("******************");
@@ -17,10 +27,16 @@ namespace LinqPractice
 
         private static void ShowFilesWithLinq(string path)
         {
-            var query = from file in new DirectoryInfo(path).GetFiles()
-                        orderby file.Length descending
-                        select file;
-            foreach(var file in query.Take(5))
+            //var query = from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file;
+            //foreach(var file in query.Take(5))
+            //{
+            //    Console.WriteLine($"{file.Name} : {file.Length}");
+            //}
+            var query = new DirectoryInfo(path).GetFiles().OrderByDescending(f => f.Length).Take(5);
+
+            foreach(var file in query)
             {
                 Console.WriteLine($"{file.Name} : {file.Length}");
             }
