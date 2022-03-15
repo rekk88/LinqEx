abbdf33754a84c84209af48adf9088fa2ca0a328
maicol <maicol.matteoli@gmail.com>
2022-03-15 16:53:50 +0100

    things

---
+++ LinqPractice/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool IsSquare(int n)
+            {
+                return Math.Sqrt(n) % 1 == 0;
+            }
+
+            IsSquare(4);
+        }
+    }
+}
