a5ff123ef42189841bbcd6c2e05ebcf7b79743f2
maicol <maicol.matteoli@gmail.com>
2022-03-17 18:20:47 +0100

    to redo

--- LinqPractice/Test/Program.cs
+++ LinqPractice/Test/Program.cs
@@ -1,17 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Test
 {
     class Program
-    {
+    { 
+      
         static void Main(string[] args)
         {
-            static bool IsSquare(int n)
+            string str = "AAAABBBCCDAABBB";
+           
+            List<string> filteredValues = new List<string>();
+
+            //int[] numbers = { 1, 2, 2, 3, 3 };
+            int savePosition = 0, i = 0;
+            string savedChar = string.Empty;
+            for (i = savePosition; i < str.Length; i++)
             {
-                return Math.Sqrt(n) % 1 == 0;
+              
+                if (i+1 < str.Length)
+                {
+                    if (str[i] == str[i + 1])
+                    {
+                        for (int j = i + 2; j < str.Length; j++)
+                        {
+                            if (str[i] != str[j])
+                            {
+                                savePosition = j-1;
+                                
+                                break;
+                            }
+                        }
+                        if(i <= savePosition) 
+                        {
+                            savedChar = string.Concat(str[i..savePosition].Distinct().TakeWhile(char.IsLetter));
+                        }
+                        
+                        i = savePosition;
+                        filteredValues.Add(savedChar.ToString());
+                    }
+                }
+                
             }
 
-            IsSquare(4);
+            Console.WriteLine("ciao");
         }
     }
 }
